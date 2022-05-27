d8e04876b339d6630f72efcec8c9eb3c9cba5325
MaguRo <64681295+MocA-Love@users.noreply.github.com>
2022-05-27 18:17:07 +0900

    しゅうせい

--- notify.py
+++ notify.py
@@ -5,53 +5,47 @@
 #
 # plz check:https://github.com/MocA-Love/Notify-Sample
 
+from requests import Response
 import requests
 
+# Don't rewrite if u r not understand :(
 class Maguro():
-    def __init__(self):
-        # Line-Notify token
-        self.token = ""
-        
+    def __init__(self, token):
+        self.token = token
         self.check_token()
-
-    def main(self) -> None:
-        """ write the process here | ここに処理を書く """
-
-        self.sendMessage("hi tuna")
 
     # send text | 文字送信
     def sendMessage(self, msg: str) -> None:
         """ msg : text | msg : 文字"""
         if not msg:
-            raise Exception("plz enter text")
-        self.post({"message": msg})
+            raise ValueError("plz enter text")
+        self._post({"message": msg})
 
     #　send sticker | スタンプ送信
     def sendSticker(self, pkg_id: str="1", stk_id: str="1", msg: str="sticker") -> None:
         """ pkg_id : package id, stk_id : sticker id, msg : sticker title
             pkg_id : パッケージID, stk_id : スタンプID, msg : スタンプタイトル """
-        self.post({"message": msg, "stickerPackageId": pkg_id, "stickerId": stk_id})
+        self._post({"message": msg, "stickerPackageId": pkg_id, "stickerId": stk_id})
 
     # send image | 画像送信
     def sendImage(self, path: str, msg: str="image") -> None:
         """ path : image path, msg : image title
             path : 画像パス,    msg : 画像タイトル """
         if not path:
-            raise Exception("plz enter path")
-        self.post({"message": msg}, {"imageFile": open(path, "rb")})
+            raise ValueError("plz enter path")
+        self._post({"message": msg}, {"imageFile": open(path, "rb")})
 
     # send url-image | URLの画像を送信
     def sendImageWithURL(self, url: str, msg: str="image") -> None:
         """ url : image url, msg : image title
             url : 画像URL     msg : 画像タイトル """
         if not url:
-            raise Exception("plz enter url")
+            raise ValueError("plz enter url")
         with open("tmp.jpg", "wb") as f:
             f.write(requests.get(url).content)
         self.post({"message": msg}, {"imageFile": open("tmp.jpg", "rb")})
 
-    # Don't rewrite if u r not understand :(
-    def check_token(self) -> None:
+    def check_token(self) -> Response:
         """ check if the token is valid """
         r = requests.get(
             url="https://notify-api.line.me/api/status",
@@ -59,9 +53,9 @@ class Maguro():
         )
         if r.status_code != 200:
             raise Exception("invalid token")
+        return r
 
-    # Don't rewrite if u r not understand :(
-    def revoke_token(self) -> None:
+    def revoke_token(self) -> Response:
         """ deactivate token """
         r = requests.post(
             url="https://notify-api.line.me/api/revoke",
@@ -69,9 +63,9 @@ class Maguro():
         )
         if r.status_code != 200:
             raise Exception("invalid token")
+        return r
 
-    # Don't rewrite if u r not understand :(
-    def post(self, payload: dict, files: dict={}) -> None:
+    def _post(self, payload: dict, files: dict={}) -> Response:
         """ request """
         r = requests.post(
             url="https://notify-api.line.me/api/notify",
@@ -81,6 +75,15 @@ class Maguro():
         )
         if r.status_code != 200:
             raise Exception("some error has occurred :(")
+        return r
+
+
+def main() -> None:
+    """ write the process here | ここに処理を書く """
+    # Line-Notify token
+    msg = Maguro("")
+    msg.sendMessage("hi tuna")
+
 
 if __name__ == "__main__":
-    Maguro().main()
+    main()
